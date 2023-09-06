371f6bbfcd298e6f45eb2066a21f88110cfea28f
kalpanakarunarathna@gmail.com <kalpanakarunarathna@gmail.com>
2023-09-06 23:26:38 +0300

    Initial commit

---
+++ src/PrintApp.java
@@ -0,0 +1,45 @@
+
+public class PrintApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String creator;
+		double number1;
+		double number2;
+		double multipication;
+		double addition;
+		double substraction;
+		double division;
+		
+		System.out.println("Printer App");
+		System.out.println("This app is made by:");
+		creator = "Chalani";
+		System.out.println("This app is made by:" + creator);
+		
+		number1=5;
+		number2=2;
+		
+		
+		//System.out.println(number1);
+		System.out.println("The value of number1 is " + number1);
+		System.out.println("The value of number2 is " + number2);
+		
+		
+		multipication = number1 * number2;
+		System.out.println(number1 + "*" + number2 + "=" + multipication);
+		
+		
+		addition = number1 + number2;
+		System.out.println(number1 + "+" + number2 + "=" + addition);
+		
+		
+		substraction= number1 - number2;
+		System.out.println(number1 + "-" + number2 + "=" + substraction );
+		
+
+		division = number1 / number2;
+		System.out.println(number1 + "/" + number2 + "=" + division);
+		
+	}
+
+}
